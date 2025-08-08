a1935c01f3976166e11b2b623625fdc2fd2818a1
GravityWavesOrg <gravitywaves2000@gmail.com>
2025-08-08 13:21:59 +0330

    🗑️ remove(modules): Deleted files updated\n\nChanges included (as of 2025-08-08 13:21:59):\n- modules/__init__.py: This file was removed from the project.\n  Summary:\n    diff --git a/modules/__init__.py b/modules/__init__.py\n    deleted file mode 100644\n    index 91df3c4..0000000\n    --- a/modules/__init__.py\n    +++ /dev/null\n\n\nPlease describe the reason or issue addressed by these changes.

--- modules/__init__.py
+++
@@ -1,4 +0,0 @@
-# This __init__.py file is used to make the modules directory a package.
-# It can be used to import submodules or subpackages for easier access.
-
-# Import subpackages for services and project_management modules
