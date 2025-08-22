9e9e4ae97f311793e7715e6043c3fa9a631716ab
GravityWavesOrg <gravitywaves2000@gmail.com>
2025-08-22 10:39:56 +0330

    Auto-commit: 2 file(s) updated

---
+++ autoprojectmanagement/api/dashboard_endpoints.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+path: autoprojectmanagement/api/dashboard_endpoints.py
+File: dashboard_endpoints.py
+Purpose: Dashboard-specific API endpoints for AutoProjectManagement
+Author: AutoProjectManagement Team
