0f52bda7060b88c13340c7a7d78ebf3d9257f91d
GravityWavesOrg <gravitywaves2000@gmail.com>
2025-08-22 12:29:11 +0330

    Auto-commit: 1 file(s) updated

---
+++ autoprojectmanagement/api/sse_endpoints.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+path: autoprojectmanagement/api/sse_endpoints.py
+File: sse_endpoints.py
+Purpose: Server-Sent Events (SSE) endpoints for real-time updates
+Author: AutoProjectManagement Team
+Version: 1.0.0
