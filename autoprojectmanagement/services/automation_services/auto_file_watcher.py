83362a7d05f83ae1d6604a4d1f89fcb58e23c25a
Shakour <81189779+Shakour-Data@users.noreply.github.com>
2025-08-23 12:41:26 +0330

    Auto-commit: 2 file(s) updated

--- autoprojectmanagement/services/automation_services/auto_file_watcher.py
+++ autoprojectmanagement/services/automation_services/auto_file_watcher.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+ز#!/usr/bin/env python3
 """
 Automatic File Watcher Service for AutoProjectManagement
 Purpose: Provides real-time file monitoring and automatic auto-commit execution
