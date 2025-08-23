0aad9ebf7ca53bd4ba2e1a5ffb9a5f6d05810a39
Shakour <81189779+Shakour-Data@users.noreply.github.com>
2025-08-23 11:28:17 +0330

    Auto-commit: 1 file(s) updated

--- autoprojectmanagement/services/configuration_cli/cli_commands.py
+++ autoprojectmanagement/services/configuration_cli/cli_commands.py
@@ -3,7 +3,7 @@ import os
 import subprocess
 import logging
 import json
-from ..main_modules.setup_initialization import initialize_git_repo, create_virtualenv, install_dependencies, create_requirements_file, ensure_gitignore_excludes_venv
+from autoprojectmanagement.main_modules.setup_initialization import initialize_git_repo, create_virtualenv, install_dependencies, create_requirements_file, ensure_gitignore_excludes_venv
 from .github_project_manager import GitHubProjectManager
 
 logger = logging.getLogger("cli_commands")
